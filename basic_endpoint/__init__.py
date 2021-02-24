22399fe02ee506c64650922bcf0ef3347771a15e
Pedro Martinho <pjemartinho@gmail.com>
2021-02-24 11:03:00 +0000

    first blueprint creation

---
+++ basic_endpoint/__init__.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, request
+
+blueprint = Blueprint('api', __name__, url_prefix='/api/')
+
+
+@blueprint.route('/users', methods=['GET', 'POST'])
+def users():
+    if request.method == "GET":
+        return {'message': 'Hello World!', 'method': request.method}
+    if request.method == "POST":
+        return {'message': 'Hello World!', 'method': request.method}
+
+
+@blueprint.route('/users/<int:user_id>', methods=['GET', 'POST'])
+def user(user_id):
+    if request.method == "GET":
+        return {'user_id': user_id, 'message': 'Hello World!', 'method': request.method}
+    if request.method == "UPDATE":
+        return {'user_id': user_id, 'message': 'Hello World!', 'method': request.method}
+    if request.method == "DELETE":
+        return {'user_id': user_id, 'message': 'Hello World!', 'method': request.method}
---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from basic_endpoint import blueprint
+
+app = Flask(__name__)
+app.register_blueprint(blueprint)
+
+# @app.route('/')
+# def base():
+#     return { 'message': 'Hello World!' }
+
+
+# @app.route('/', methods=['GET', 'POST'])
+# def base():
+#     if request.method == "GET":
+#         return { 'message': 'Hello World!', 'method': request.method }
+#     if request.method == "POST":
+#         return { 'message': 'Hello World!', 'method': request.method }
+
+
