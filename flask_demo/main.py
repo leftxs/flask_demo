e28468304d77f6c4a95d858590a8978886788a19
Pedro Martinho <pjemartinho@gmail.com>
2021-02-24 12:41:53 +0000

    add documentation to endpoints - work in progress

--- flask_demo/main.py
+++ flask_demo/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_demo.basic_endpoint import blueprint
+from flask_demo.docummented_endpoints import blueprint as v1
 
 app = Flask(__name__)
-app.register_blueprint(blueprint)
+app.register_blueprint(v1)
+# app.register_blueprint(blueprint)
 
 # @app.route('/')
 # def base():
