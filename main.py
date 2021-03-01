cc8934d3989a22fdfb41c16a0ea76ee380d98fbd
Pedro Martinho <pjemartinho@gmail.com>
2021-03-01 15:18:47 +0000

    remove commented code

--- main.py
+++ main.py
@@ -5,21 +5,10 @@ from blueprints.documented_endpoints import blueprint as documented_endpoint
 
 app = Flask(__name__)
 app.config['RESTPLUS_MASK_HEADER'] = False
+
 app.register_blueprint(basic_endpoint)
 app.register_blueprint(jinja_template_blueprint)
 app.register_blueprint(documented_endpoint)
-
-# @app.route('/')
-# def base():
-#     return { 'message': 'Hello World!' }
-
-
-# @app.route('/', methods=['GET', 'POST'])
-# def base():
-#     if request.method == "GET":
-#         return { 'message': 'Hello World!', 'method': request.method }
-#     if request.method == "POST":
-#         return { 'message': 'Hello World!', 'method': request.method }
 
 if __name__ == "__main__":
     app.run()
